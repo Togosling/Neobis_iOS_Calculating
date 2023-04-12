9181d38d7b50a981c1fedd5e75d457edaa9a1935
Togosling <114942562+Togosling@users.noreply.github.com>
2023-04-12 16:33:25 +0800

    first commit

---
+++ Neobis_iOS_Calculating/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_Calculating
+//
+//  Created by Тагай Абдылдаев on 12/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
